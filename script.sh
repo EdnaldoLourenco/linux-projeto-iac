5fcc7a357c6ee4c970c504593cfd78d75bd26f93
Ubuntu <ednaldo-devops-labs@VM-DevOps-Labs.oa1uitgx3zruhkr32fgzqs3y5g.bx.internal.cloudapp.net>
2023-08-29 01:39:55 +0000

    Apache v.1.0

---
+++ script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Atualizando o servidor..."
+
+apt-get update
+apt-get upgrade -y
+
+echo "Instalando o apache2..."
+
+apt install apache2 -y
+
+echo "Instalando o unzip..."
+
+apt install unzip -y
+
+echo "Baixando e copiando aquivis da aplicação..."
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
